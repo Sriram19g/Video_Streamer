787375a5682fdf5756f61944170fa70bb25225ce
Sriram19g <sriramg.cs2023@citchennai.net>
2024-08-28 18:11:07 +0530

    [CREATE] create client_socket

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
-import socket,cv2,picket, struct
+import socket,cv2,pickle, struct
+client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host_ip=input("Enter the HOST_IP     : ")
+port=input("Enter the port number : ")
+
+socket_addr=(host_ip,port)
+
+client_socket.connect(socket_addr)
+
