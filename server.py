324b830f7868e5621be0d2753d1e97fddc34f04c
Sriram19g <sriramg.cs2023@citchennai.net>
2024-08-28 17:54:58 +0530

    [WRITE] writing server.py

--- server.py
+++ server.py
@@ -12,5 +12,22 @@ server_socket.bind(socket_addr)
 server.socket.listen(5)
 print("LISTENING AT: ",socket_addr)
 
+while True:
+    client_socket,addr=server_socket.accept()
+    print('GOT CONNECTION FROM :',addr)
+    if client_socket:
+        vid=cv2.VideoCapture(0)
+        while(vid.isOpened()):
+            img,frame=vid.read()
+            a=pickle.dumps(frame)
+            message=struct.pack("Q",len(a))+a
+            client_socket.sendall(message)
+            cv2.imshow("TRANSMITTING VIDEO",frame)
+            key=cv2.waitKey(1) & 0xFF
+            if(key==org('q')):
+                client_socket.close()
+                
+
+
 
 
