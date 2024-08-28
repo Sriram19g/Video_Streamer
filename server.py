ea84f1c45954047bea1f1a20a4e38e0bc786f94e
Sriram19g <sriramg.cs2023@citchennai.net>
2024-08-28 17:44:42 +0530

    [MODIFY] adding socket address

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
 import socket,cv2,pickle,struct
+
+server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host_name=socket.gethostbyname()
+host_ip=socket.gethostbyname(host_name)
+print("HOST IP : ",host_ip)
+port=8888
+socket_addr=(host_ip,port)
+
+server_socket.bind(socket_addr)
+server.socket.listen(5)
+print("LISTENING AT: ",socket_addr)
+
+
+
