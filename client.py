931e8b7f3be16243a3f21bed1d94fa712085c221
Sriram19g <sriramg.cs2023@citchennai.net>
2024-08-28 18:26:11 +0530

    [WRITE] code for getting data

--- client.py
+++ client.py
@@ -8,3 +8,24 @@ socket_addr=(host_ip,port)
 
 client_socket.connect(socket_addr)
 
+data=b""
+
+payload_size=struct.calcsize("Q")
+
+while True:
+    while len(data) < payload_size:
+        packet=client_socket.recv(4*1024)  ##4KB
+        if not packet: break
+        data+=packet
+    
+    packet_msg_size=data[:payload_size]
+    data=data[payload_size:]
+    msg_size=struct.unpack("Q",packed_msg_size)[0]
+
+    while len(data)<msg_size:
+        data+=client_socket.recv(4*1024)
+    
+    frame_data = data [:msg_size]
+    data=data[msg_size:]
+
+    
