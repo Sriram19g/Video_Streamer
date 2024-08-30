48b92c354ecb2f76be660f6ff62b812ddcd7c4b2
Sriram19g <sriramg.cs2023@citchennai.net>
2024-08-30 17:19:32 +0530

    [VIDEO] testing CV2

--- UDP streamer/server.py
+++ UDP streamer/server.py
@@ -3,6 +3,31 @@ import cv2,socket,time,base64,numpy as np
 BUFF_SIZE=65536
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+
 host_name=socket.gethostname()
 host_ip=socket.gethostbyname(host_name)
-print(host_ip)
+port=2006
+socket_addr=(host_ip,port)
+server_socket.bind(socket_addr)
+print(f"Listening at : {socket_addr}")
+
+vid=cv2.VideoCapture('nature.mp4')
+if not vid.isOpened():
+    print("Can't Open file")
+
+fps,st,frame_to_count,cnt=(0,0,20,0)
+
+while True:
+    ret,frame=vid.read()
+    if not ret: 
+        break
+    height=1000
+    width=1000
+    cv2.resize(frame,(height,width))
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
